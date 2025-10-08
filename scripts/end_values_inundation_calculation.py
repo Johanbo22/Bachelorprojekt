1e951882718e51214f0ae941c15342acecda6bb2
Johan Bo Kjær <jbk@famkjaer.dk>
2025-10-08 12:38:19 +0200

    Update end_values_inundation_calculation.py

--- scripts/end_values_inundation_calculation.py
+++ scripts/end_values_inundation_calculation.py
@@ -1,24 +1,19 @@
 import math
 
-'''
-Funktionalitet til at udregne end values og num of iterations i Create Inundation værktøjet fra Inundation Modellen
-'''
+def iteration(end: int, initial: int, increment: int) -> int:
+    iterations: float = (end - initial) / increment + 1
+    iter: int = math.ceil(iterations)
 
+    return iter
 
-def iteration(end, initial, increment):
-    iterations = (end - initial) / increment + 1
-    iter = math.ceil(iterations)
-
-    return  iter
-
-def end_val(iter, initial, increment):
+def end_val(iter: int, initial: int, increment: int) -> int:
     if iter == 0:
         return initial
-    end = initial + ((iter - 1) * increment)
+    end: int = initial + ((iter - 1) * increment)
 
     return end
 
-iter_result = iteration(200, 100, 1)
+iter_result = iteration(end=246, initial=100, increment=1)
 print(f"Antal iterationer: {iter_result}")
 
 end_result = end_val(iter_result, 100, 1)
